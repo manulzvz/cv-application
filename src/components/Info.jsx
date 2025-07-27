29b58b6b11d36373219491601a53915519ed583c
manulzvz13 <manulzvz@gmail.com>
2025-07-27 17:52:51 -0600

    feat: State management and transmission validation of GeneralInfo

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,28 +1,66 @@
-function Info() {
+function Info({generalInfo, setGeneralInfo}) {
+    
+  function handleChange(e) {
+    const {name, value} = e.target;
+    setGeneralInfo(prev => ({
+        ...prev,
+        [name]:value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(generalInfo);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>
           Nombre Completo:
-          <input type="text" name="fullName" required />
+          <input 
+            type="text" 
+            name="fullName"
+            required
+            value={generalInfo.fullName}
+            onChange={handleChange}
+          />
         </label>
       </div>
       <div>
         <label>
           Fecha de Nacimiento:
-          <input type="date" name="birthDate" required />
+          <input 
+            type="date"
+            name="birthDate"
+            required
+            value={generalInfo.birthDate}
+            onChange={handleChange} 
+          />
         </label>
       </div>
       <div>
         <label>
           Email:
-          <input type="email" name="email" required />
+          <input 
+            type="email"
+            name="email" 
+            required 
+            value={generalInfo.email}
+            onChange={handleChange}
+          />
         </label>
       </div>
       <div>
         <label>
           Phone:
-          <input type="tel" name="phone" required />
+          <input 
+            type="tel" 
+            name="phone"
+            required
+            value={generalInfo.phone}
+            onChange={handleChange} 
+          />
         </label>
       </div>
       <button type="submit">Guardar</button>
